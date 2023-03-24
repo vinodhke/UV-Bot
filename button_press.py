490b7cf796c9cef60d75b254c593c220cf7589f5
vinodhke <89589445+vinodhke@users.noreply.github.com>
2023-03-24 16:42:29 -0400

    Update button_press.py

--- button_press.py
+++ button_press.py
@@ -2,17 +2,18 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 def on_button_press(channel):
-    print(True)
+    if GPIO.input(channel) == GPIO.LOW:
+        print(True)
 
 button_pin = 2  # Assuming the button is connected to GPIO2 (Pin 3)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=on_button_press, bouncetime=300)
-
 try:
     while True:
-        sleep(1)
+        GPIO.wait_for_edge(button_pin, GPIO.FALLING)
+        sleep(0.01)  # Adding a debounce time
+        on_button_press(button_pin)
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
